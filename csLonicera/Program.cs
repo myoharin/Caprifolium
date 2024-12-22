9c6ceeeb5ff84812b880e3505bb3277fdb002b37
MyoHarin <myoharin@gmail.com>
2024-12-22 04:29:46 +0000

    initial commit

---
+++ csLonicera/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualBasic;
+using SineVita.Lonicera;
+
+Func<int, int, int> multiply = (x, y) => x * y;
+Func<int, int, Tuple<int,int>> combine = (x, y) => new Tuple<int, int>(x,y);
+Console.WriteLine("start");
+Lonicera<int, Tuple<int,int>> loniTest = new Lonicera<int, Tuple<int,int>>(combine);
+void print(){
+    int index = 0;
+    Console.WriteLine(loniTest.Links.Count());
+    for (int i = 1; i < loniTest.NodeCount; i++) {
+        for (int j = 0; j < i; j++) {
+            Console.Write($"| {loniTest.Links[index]} ");
+            index++;
+        }
+        Console.Write("\n");
+    }
+    Console.WriteLine();
+    foreach(var link in loniTest.Links) {
+        Console.Write($"| {link} ");
+    }
+    Console.WriteLine();
+}
+void PrintList(List<int> list){
+    foreach (int item in list) {
+        Console.Write(item + ", ");
+    }
+    Console.WriteLine();
+}
+
+
+for (int i = 1; i< 10; i++) {
+    Console.WriteLine($"({i}, {Lonicera<int, int>.NodesToLinkIndex(0,i)})");
+}
+
+loniTest.Add(0);
+loniTest.Add(1);
+loniTest.AddRange(new List<int>(){2,3,4,5,6,7,8,9});
+
+print();
+loniTest.Remove(3);
+print();
+
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+Console.WriteLine("Original List:");
+PrintList(numbers);
+numbers.RemoveRange(3, 3);
+Console.WriteLine("After RemoveRange:");
+PrintList(numbers);
---
+++ csLonicera/src/Lonicera.cs
@@ -0,0 +1,150 @@
+using System.Dynamic;
+
+namespace SineVita.Lonicera {
+
+    public class Lonicera<Node, Link> {
+        protected List<Node> _nodes;
+        protected List<Link> _links;
+        public Func<Node, Node, Link>? Growth { get; set; } // (n0, n1) - n0 < n1
+        public bool GrowthSynced = false;
+
+        public int NodeCount { get {return _nodes.Count();} }
+        public int LinkCount { get {return _links.Count();} }
+        public Node? RootNode { get {
+            if (NodeCount > 0) {return _nodes[0];} 
+            else {return default(Node);}}}   
+        public IReadOnlyList<Node> Nodes => _nodes.AsReadOnly();
+        public IReadOnlyList<Link> Links => _links.AsReadOnly();
+
+        public Lonicera(Func<Node, Node, Link>? growth = null, bool grow = false, List<Node>? nodes = null, List<Link>? links = null) {
+            Growth = growth;
+            if (nodes != null) {_nodes = nodes;} else {_nodes = new List<Node>();}
+            if (links != null) {_links = links;} else {_links = new List<Link>();}
+
+            for (int i = 0; i < CalculateVineCount(); i++) {
+                _links[i] = default(Link);
+            }
+            if (Growth != null && grow) {Grow();}
+        }
+
+        public int CalculateVineCount() {return (int)Math.Floor(NodeCount * (NodeCount + 1) * 0.5f);}       
+        public Link GetValue(int n0, int n1) {
+            if (n0 >= NodeCount || n1 >= NodeCount) {
+                throw new ArgumentOutOfRangeException("");
+            }
+            return _links[NodesToLinkIndex(n0, n1)];
+        }
+
+        // * Node Insertion Function
+        public void Add(Node newNode, bool growNew = true) {
+            for (int i = 0; i < NodeCount; i++) {
+                if (growNew && Growth != null) {
+                    _links.Add(Growth(_nodes[i], newNode));
+                }
+                else {
+                    _links.Add(default(Link));
+                }
+            }
+
+            
+            _nodes.Add(newNode);
+        }
+        public void Insert(int index, Node newNode, bool growNew = true) {
+            _nodes.Insert(index, newNode);
+            int rowPivot;
+            if (index == 0) {rowPivot = 0;} 
+            else {rowPivot = NodesToLinkIndex(0, index-1);}
+            
+            // add rows
+            Link cacheLink = default(Link);
+            for (int i = 0; i < index; i++) {
+                if (growNew && Growth != null) {cacheLink = Growth(_nodes[i], _nodes[index]);}
+                _links.Insert(rowPivot+i, cacheLink);
+            }
+
+            // add collumns
+            for (int i = index+1; i < NodeCount+1; i++) {
+                if (growNew && Growth != null) {cacheLink = Growth(_nodes[index], _nodes[i]);}
+                _links.Insert(NodesToLinkIndex(index, i), cacheLink);
+            }
+        }
+        public void AddRange(List<Node> newNodeRange, bool growNew = true) {
+            foreach (Node node in newNodeRange) {
+                Add(node, growNew);
+            }
+        }
+        public void InsertRange(int index, List<Node> newNodeRange, bool growNew = true) {
+            foreach (Node node in newNodeRange) {
+                Insert(index, node, growNew);
+                index++;
+            }
+        }
+        
+        public bool Remove(Node target) {
+            for (int i = 0; i < NodeCount; i++) {
+                if (_nodes[i] != null) {
+                    if (_nodes[i]!.Equals(target)) {
+                        RemoveAt(i);
+                        return true; 
+                    }
+                }
+            }
+            return false;
+        }
+        public void RemoveAt(int index) {
+            // remove row
+            if (index != 0) {
+                _links.RemoveRange(NodesToLinkIndex(0, index), index);
+            }
+            // remove collumn
+            for (int i = 0; i < NodeCount-index-1; i++) {
+                _links.RemoveAt(NodesToLinkIndex(0, i+index+1)-i);
+            }
+            // remove from node list
+            _nodes.RemoveAt(index);
+        }
+        public void RemoveRange(int index, int count) {
+            for (int _ = 0; _ < count; _++) {
+                RemoveAt(index);
+            }
+        }
+
+
+        public void Update_nodes(Func<Node, Node> func) {
+            GrowthSynced = false;
+            for (int i = 0; i < NodeCount; i++) {_nodes[i] = func(_nodes[i]);}
+        }
+        public void Update_links(Func<Link, Link> func) {
+            GrowthSynced = false;
+            for (int i = 0; i < LinkCount; i++) {_links[i] = func(_links[i]);}
+        }
+
+        public void Grow() {
+            if (NodeCount <= 1 || Growth == null) {return;}
+            GrowthSynced = true;
+            int index = 0;
+            for (int i = 0; i < NodeCount; i++) {
+               for (int j = i+1; j < NodeCount; j++) {
+                    _links[index] = Growth(_nodes[i], _nodes[j]);
+                } 
+            }
+        }
+        public void Clear() {
+            _nodes.Clear();
+            _links.Clear();
+        }
+
+        
+        // * static functions
+        public static int NodesToLinkIndex(int n0, int n1) {
+            if (n1 < n0) {
+                int n = n1;
+                n1 = n0;
+                n0 = n;
+            }
+            if (n1 == n0) {
+                throw new ArgumentException("The two node index cannot be the same.");
+            }
+            return (int)Math.Floor(n1 * (n1 - 1) * 0.5f) + n0;}
+    }
+}
